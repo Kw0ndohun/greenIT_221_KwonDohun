3445d9cc54b5c5d5e46c902c9fb34e788318a4d2
Administrator <Administrator@322-01>
2022-06-22 12:21:20 +0900

    고켓몬으로 변경

--- rpg_final_test/src/finalTestMolyGame/Stage.java
+++
@@ -1,7 +0,0 @@
-package finalTestMolyGame;
-
-public class Stage {
-	//랜덤세팅된 몬스터리스트를 받는 어레이 리스트 필요
-	//ㅇㅇ
-	
-}
--- rpg_final_test/src/goketmonGame/Shop.java
+++ rpg_final_test/src/goketmonGame/Shop.java
@@ -1,6 +1,7 @@
 package goketmonGame;
 
 public class Shop {
+	//아이템매니저 아이템 리스트가져오기
 	//몬스터볼 판매
 	//회복아이템
 }
